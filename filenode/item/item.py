fc75148c077888ae9f478164bf97c7e6d6f0ea04
adeadfed <maksym.vatsyk17@gmail.com>
2024-02-04 12:21:51 +0100

    pad optional header offset field

--- filenode/item/item.py
+++ filenode/item/item.py
@@ -9,6 +9,7 @@ class Item:
     def to_bytes(self):
         item_bytes = b''
         item_bytes += self.header.to_bytes()
+        item_bytes += bytes(self.header.t_hoff - len(self.header.to_bytes()))
         item_bytes += self.data
 
         return item_bytes
